3000a0b6333b977df3f0d7a1ef4904021aa425a1
Steven Borbor <steven.borbor@jelou.ai>
2025-01-01 12:59:11 -0500

    se añadio la seccion especialista en creacion

---
+++ noetec-web/src/data/logos.js
@@ -0,0 +1,11 @@
+import Logo1 from "../assets/logo1.png";
+import Logo2 from "../assets/logo2.png";
+import Logo3 from "../assets/logo3.png";
+import Logo4 from "../assets/logo4.png";
+import Logo5 from "../assets/logo5.png";
+import Logo6 from "../assets/logo6.png";
+import Logo7 from "../assets/logo7.png";
+
+const logos = [Logo1, Logo2, Logo3, Logo4, Logo5,Logo6,Logo7];
+
+export default logos;
--- noetec-web/src/pages/Home/Home.jsx
+++ noetec-web/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
+import EspecialistaCreacion from "./EspecialistaCreacion";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <EspecialistaCreacion/>
     </>
   );
 };
